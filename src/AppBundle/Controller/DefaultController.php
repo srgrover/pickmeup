331841dbb0dd18681a8d2cf6caae0805e777080b
jonathan <djgrovezone@gmail.com>
2017-03-16 16:12:16 +0100

    Eliminadas referencias a un Bundle eliminado anteriormente y añadido getManager() y repositorio de entidad de usuario

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,26 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $user_repo = $em->getRepository("AppBundle:Usuario");
+
+        $user = $user_repo->find(1);
+
+        echo "Bienvenido ".$user->getNombre()." ".$user->getApellidos();
+
+        //var_dump($user);
+        die();
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render(':default:index.html.twig');
     }
 }
