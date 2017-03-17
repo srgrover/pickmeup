ae8b16df9f1aed30b15e757b23bdab542c6c03b3
jonathan <djgrovezone@gmail.com>
2017-03-17 15:24:33 +0100

    Añadido formulario de registro con bootstrap, añadida plantilla de registro  y editado security.yml para poner seguridad bcrypt

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\RegisterType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Usuario;
 
 class UserController extends Controller{
 
@@ -12,8 +14,22 @@ class UserController extends Controller{
      * @Route("/login", name="login")
      */
     public function LoginAction(Request $request){
-        echo "Acción Login";
-        die();
+        return $this->render(':default:login.html.twig', array(
+            "titulo" => "Login"
+        ));
+    }
+
+    /**
+     * @Route("/register", name="Register")
+     */
+    public function RegisterAction(Request $request){
+
+        $user = new Usuario();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        return $this->render(':default:register.html.twig', array(
+            "form" => $form->createView()
+        ));
     }
 
 }
---
+++ src/AppBundle/Form/RegisterType.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class RegisterType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre', TextType::class, array(
+                'label' => 'Nombre',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-name form-control'
+                )
+            ))
+            ->add('apellidos', TextType::class, array(
+                'label' => 'Apellidos',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-surname form-control'
+                )
+            ))
+            ->add('username', TextType::class, array(
+                'label' => 'Nick',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-nick form-control nick-input'
+                )
+            ))
+            ->add('email', EmailType::class, array(
+                'label' => 'Correo electrónico',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-email form-control'
+                )
+            ))
+            ->add('password', PasswordType::class, array(
+                'label' => 'Contraseña',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-password form-control'
+                )
+            ))
+            ->add('fechaNacimiento', DateType::class, array(
+                'label' => 'Fecha de nacimiento',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-date form-control'
+                )
+            ))
+            ->add('ciudad', TextType::class, array(
+                'label' => 'Ciudad',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-city form-control'
+                )
+            ))
+            ->add('telefono', TextType::class, array(
+                'label' => 'Teléfono',
+                'required' => 'required',
+                'attr' => array(
+                    'class' => 'form-tlf form-control'
+                )
+            ))
+            ->add('Registrarse', SubmitType::class, array(
+                "attr" => array(
+                    "class" => "form-submit btn btn-success"
+                )
+            ))
+       ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Usuario'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_usuario';
+    }
+
+
+}
