758227044db599bbff4a624a91897494e795c2fa
jonathan <djgrovezone@gmail.com>
2017-03-22 15:46:39 +0100

    Añadido sistema para dejar de seguir a una persona y mostrar u ocultar el boton de seguir o dejar de seguir según corresponda (añadiendo una extension twig)

--- src/AppBundle/Controller/SeguimientoController.php
+++ src/AppBundle/Controller/SeguimientoController.php
@@ -23,7 +23,7 @@ class SeguimientoController extends Controller{
     /**
      * @Route("/follow", name="following_follow", methods="POST")
      */
-    public function seguimientoAction(Request $request){
+    public function followAction(Request $request){
         $user = $this->getUser();                   //Obtenemos el usuario actual
         $followed_id = $request->get('followed');   //Obtenemos el usuario al que vamos a seguir. 'followed' es una variable recogida por POST
 
@@ -44,6 +44,36 @@ class SeguimientoController extends Controller{
             $status = "Ahora estas siguiendo a este usuario.";
         }else{
             $status = "No se ha podido seguir a este usuario.";
+        }
+
+        return new Response($status);
+    }
+
+
+    /**
+     * @Route("/unfollow", name="following_unfollow", methods="POST")
+     */
+    public function unfollowAction(Request $request){
+        $user = $this->getUser();                   //Obtenemos el usuario actual
+        $followed_id = $request->get('followed');   //Obtenemos el usuario al que vamos a seguir. 'followed' es una variable recogida por POST
+
+        $em = $this->getDoctrine()->getManager();
+
+        $following_repo = $em->getRepository('AppBundle:Seguimiento');
+
+        $followed = $following_repo->findOneBy(array(
+            "usuario"=> $user,
+            "seguidor" => $followed_id
+        ));
+
+
+        $em->remove($followed);
+        $flush = $em->flush();  //Para que guarde los cambios
+
+        if($flush == null){     //Si no da fallo
+            $status = "Has dejado de seguir a este usuario.";
+        }else{
+            $status = "No se ha podido dejar de seguir a este usuario.";
         }
 
         return new Response($status);
--- web/assets/js/custom/users.js
+++ web/assets/js/custom/users.js
@@ -34,8 +34,23 @@ $(document).ready(function () {
 
 function followButtons() {
     $(".btn-follow").unbind("click").click(function () {
+        $(this).addClass("hidden");
+        $(this).parent().find(".btn-unfollow").removeClass("hidden");
         $.ajax({
             url: URL+'/follow',
+            type: 'POST',
+            data: {followed: $(this).attr("data-followed")},    //followed es la variable que se le pasará por POST
+            success: function (response) {
+                console.log(response);
+            }
+        });
+    });
+
+    $(".btn-unfollow").unbind("click").click(function () {
+        $(this).addClass("hidden");
+        $(this).parent().find(".btn-follow").removeClass("hidden");
+        $.ajax({
+            url: URL+'/unfollow',
             type: 'POST',
             data: {followed: $(this).attr("data-followed")},    //followed es la variable que se le pasará por POST
             success: function (response) {
